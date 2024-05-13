93417b4f411bf963ab84c529bc9060689498eb9b
viktoriyalutsenko <viktoriyalutsenko2004@mail.ru>
2024-05-14 02:23:54 +0300

    Создание страницы "Каталог"

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.glavnaya, name='glavnaya'),  # Путь для страницы главная
+    path('catalog/', views.catalog, name='catalog'),
     path('contacts/', views.contacts, name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 def glavnaya(request):
     return render(request, 'glavnaya.html')
 
+def catalog(request):
+    return render(request, 'catalog.html')
+
 def contacts(request):
     return render(request, 'contacts.html')
+
 # Create your views here.
