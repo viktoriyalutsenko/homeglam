d4ce811d8bd6deba207ffd088f00ad7825db5dfe
viktoriyalutsenko <viktoriyalutsenko2004@mail.ru>
2024-05-17 19:37:17 +0300

    Создание админ-панели

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class Admin(AbstractUser):
+    # Добавьте дополнительные поля, если необходимо
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
