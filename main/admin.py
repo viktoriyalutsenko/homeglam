14abad383b086298cbeef9edea3dd2b2812275bc
viktoriyalutsenko <viktoriyalutsenko2004@mail.ru>
2024-05-18 20:30:55 +0300

    Создание модели "Категории"

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Admin
+from .models import Admin, Category
 
 admin.site.register(Admin)
+
+admin.site.register(Category)
--- main/models.py
+++ main/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
 
 class Admin(AbstractUser):
     # Добавьте дополнительные поля, если необходимо
     phone = models.CharField(max_length=20, null=True, blank=True)
     address = models.TextField(null=True, blank=True)
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='categories', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
