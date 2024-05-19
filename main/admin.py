0eba6969da1afb71615bd66b7299e24c8ded31f9
viktoriyalutsenko <viktoriyalutsenko2004@mail.ru>
2024-05-19 03:21:00 +0300

    Создание модели "Товары"

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Admin, Category
+from .models import Admin, Category, Product
 
 admin.site.register(Admin)
-
 admin.site.register(Category)
+admin.site.register(Product)
--- main/models.py
+++ main/models.py
@@ -13,3 +13,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
