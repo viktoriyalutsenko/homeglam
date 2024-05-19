d43c029becddc731b9a6d6ec126d9538cd3fc89b
viktoriyalutsenko <viktoriyalutsenko2004@mail.ru>
2024-05-19 04:30:11 +0300

    Вывод категорий на страницу

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.glavnaya, name='glavnaya'),  # Путь для страницы главная
+    path('', views.glavnaya, name='glavnaya'),
     path('catalog/', views.catalog, name='catalog'),
+    path('catalog/<int:category_id>/', views.category_page, name='catalog-page'),
     path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Category
 # Create your views here.
 def glavnaya(request):
     return render(request, 'glavnaya.html')
@@ -14,3 +14,16 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 # Create your views here.
+def catalog(request):
+    categories = Category.objects.all()[:4]  # Получаем первые 4 категории
+    context = {
+        'categories': categories
+    }
+    return render(request, 'catalog.html', context)
+
+def category_page(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    context = {
+        'category': category
+    }
+    return render(request, 'category_page.html', context)
